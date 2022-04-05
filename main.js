5203b5abb13c2da4151e059f02edbd7631d5601f
Ahmed Shahriar <shahriar0247@gmail.com>
2022-04-05 14:39:37 +0000

    fixing bugs, adding buttons back

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
-import './style.css';
+import "./style.css";
 
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import * as firebaseConfig_file from './firebase_config.json';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import * as firebaseConfig_file from "./firebase_config.json";
 
 const firebaseConfig = {
-  apiKey:firebaseConfig_file.apiKey ,
-  authDomain:firebaseConfig_file.authDomain ,
-  projectId:firebaseConfig_file.projectId ,
-  storageBucket:firebaseConfig_file.storageBucket ,
-  messagingSenderId:firebaseConfig_file.messagingSenderId ,
-  appId:firebaseConfig_file.appId ,
+    apiKey: firebaseConfig_file.apiKey,
+    authDomain: firebaseConfig_file.authDomain,
+    projectId: firebaseConfig_file.projectId,
+    storageBucket: firebaseConfig_file.storageBucket,
+    messagingSenderId: firebaseConfig_file.messagingSenderId,
+    appId: firebaseConfig_file.appId,
 };
 
-
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
 }
 const firestore = firebase.firestore();
 
 const servers = {
-  iceServers: [
-    {
-      urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'],
-    },
-  ],
-  iceCandidatePoolSize: 10,
+    iceServers: [
+        {
+            urls: ["stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"],
+        },
+    ],
+    iceCandidatePoolSize: 10,
 };
 
 // Global State
@@ -34,140 +33,143 @@ let localStream = null;
 let remoteStream = null;
 
 // HTML elements
-const webcamVideo = document.getElementById('webcamVideo');
-const callButton = document.getElementById('callButton');
-const callInput = document.getElementById('callInput');
-const answerButton = document.getElementById('answerButton');
-const remoteVideo = document.getElementById('remoteVideo');
-const hangupButton = document.getElementById('hangupButton');
+const webcamVideo = document.getElementById("webcamVideo");
+const callButton = document.getElementById("callButton");
+const callInput = document.getElementById("callInput");
+const answerButton = document.getElementById("answerButton");
+const remoteVideo = document.getElementById("remoteVideo");
+const hangupButton = document.getElementById("hangupButton");
 
+webcamButton.onclick = async () => {
+    localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    remoteStream = new MediaStream();
 
-  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-  remoteStream = new MediaStream();
-
-  // Push tracks from local stream to peer connection
-  localStream.getTracks().forEach((track) => {
-    pc.addTrack(track, localStream);
-  });
-
-  // Pull tracks from remote stream, add to video stream
-  pc.ontrack = (event) => {
-    event.streams[0].getTracks().forEach((track) => {
-      remoteStream.addTrack(track);
+    // Push tracks from local stream to peer connection
+    localStream.getTracks().forEach((track) => {
+        pc.addTrack(track, localStream);
     });
-  };
 
-  webcamVideo.srcObject = localStream;
-  remoteVideo.srcObject = remoteStream;
+    // Pull tracks from remote stream, add to video stream
+    pc.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+            remoteStream.addTrack(track);
+        });
+    };
 
-  callButton.disabled = false;
-  answerButton.disabled = false;
+    webcamVideo.srcObject = localStream;
+    remoteVideo.srcObject = remoteStream;
+
+    callButton.disabled = false;
+    answerButton.disabled = false;
+    webcamButton.disabled = true;
+};
 
 // 2. Create an offer
 callButton.onclick = async () => {
-  // Reference Firestore collections for signaling
-  const callDoc = firestore.collection('calls').doc();
-  const offerCandidates = callDoc.collection('offerCandidates');
-  const answerCandidates = callDoc.collection('answerCandidates');
+    // Reference Firestore collections for signaling
+    const callDoc = firestore.collection("calls").doc();
+    const offerCandidates = callDoc.collection("offerCandidates");
+    const answerCandidates = callDoc.collection("answerCandidates");
 
-  callInput.value = callDoc.id;
-  let url = location.href + "?answer=" + callInput.value
-  navigator.clipboard.writeText(url)
-  error.innerText = "Send link to your lover"
+    callInput.value = callDoc.id;
+    let url = location.href + "?answer=" + callInput.value;
+    navigator.clipboard.writeText(url);
+    error.innerText = "Send link to your lover";
 
-  // Get candidates for caller, save to db
-  pc.onicecandidate = (event) => {
-    event.candidate && offerCandidates.add(event.candidate.toJSON());
-  };
+    // Get candidates for caller, save to db
+    pc.onicecandidate = (event) => {
+        event.candidate && offerCandidates.add(event.candidate.toJSON());
+    };
 
-  // Create offer
-  const offerDescription = await pc.createOffer();
-  await pc.setLocalDescription(offerDescription);
+    // Create offer
+    const offerDescription = await pc.createOffer();
+    await pc.setLocalDescription(offerDescription);
 
-  const offer = {
-    sdp: offerDescription.sdp,
-    type: offerDescription.type,
-  };
+    const offer = {
+        sdp: offerDescription.sdp,
+        type: offerDescription.type,
+    };
 
-  await callDoc.set({ offer });
+    await callDoc.set({ offer });
 
-  // Listen for remote answer
-  callDoc.onSnapshot((snapshot) => {
-    const data = snapshot.data();
-    if (!pc.currentRemoteDescription && data?.answer) {
-      const answerDescription = new RTCSessionDescription(data.answer);
-      pc.setRemoteDescription(answerDescription);
-    }
-  });
-
-  // When answered, add candidate to peer connection
-  answerCandidates.onSnapshot((snapshot) => {
-    snapshot.docChanges().forEach((change) => {
-      if (change.type === 'added') {
-        const candidate = new RTCIceCandidate(change.doc.data());
-        pc.addIceCandidate(candidate);
-      }
+    // Listen for remote answer
+    callDoc.onSnapshot((snapshot) => {
+        const data = snapshot.data();
+        if (!pc.currentRemoteDescription && data?.answer) {
+            const answerDescription = new RTCSessionDescription(data.answer);
+            pc.setRemoteDescription(answerDescription);
+        }
     });
-  });
 
-  hangupButton.disabled = false;
+    // When answered, add candidate to peer connection
+    answerCandidates.onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+            if (change.type === "added") {
+                const candidate = new RTCIceCandidate(change.doc.data());
+                pc.addIceCandidate(candidate);
+            }
+        });
+    });
 };
 
 // 3. Answer the call with the unique ID
 answerButton.onclick = async () => {
-  const callId = callInput.value;
-  const callDoc = firestore.collection('calls').doc(callId);
-  const offerCandidates = callDoc.collection('offerCandidates');
-  const answerCandidates = callDoc.collection('answerCandidates');
+    const callId = callInput.value;
+    const callDoc = firestore.collection("calls").doc(callId);
+    const offerCandidates = callDoc.collection("offerCandidates");
+    const answerCandidates = callDoc.collection("answerCandidates");
 
-  pc.onicecandidate = event => {
-    event.candidate && answerCandidates.add(event.candidate.toJSON());
-  };
+    pc.onicecandidate = (event) => {
+        event.candidate && answerCandidates.add(event.candidate.toJSON());
+    };
 
-  // Fetch data, then set the offer & answer
+    // Fetch data, then set the offer & answer
 
-  const callData = (await callDoc.get()).data();
+    const callData = (await callDoc.get()).data();
 
-  const offerDescription = callData.offer;
-  await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
+    const offerDescription = callData.offer;
+    await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
 
-  const answerDescription = await pc.createAnswer();
-  await pc.setLocalDescription(answerDescription);
+    const answerDescription = await pc.createAnswer();
+    await pc.setLocalDescription(answerDescription);
 
-  const answer = {
-    type: answerDescription.type,
-    sdp: answerDescription.sdp,
-  };
+    const answer = {
+        type: answerDescription.type,
+        sdp: answerDescription.sdp,
+    };
 
-  await callDoc.update({ answer });
+    await callDoc.update({ answer });
 
-  // Listen to offer candidates
+    // Listen to offer candidates
 
-  offerCandidates.onSnapshot((snapshot) => {
-    snapshot.docChanges().forEach((change) => {
-      console.log(change)
-      if (change.type === 'added') {
-        let data = change.doc.data();
-        pc.addIceCandidate(new RTCIceCandidate(data));
-      }
+    offerCandidates.onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+            console.log(change);
+            if (change.type === "added") {
+                let data = change.doc.data();
+                pc.addIceCandidate(new RTCIceCandidate(data));
+            }
+        });
     });
-  });
 };
 
-
-
 function findGetParameter(parameterName) {
-  var result = null,
-      tmp = [];
-  location.search
-      .substr(1)
-      .split("&")
-      .forEach(function (item) {
-        tmp = item.split("=");
-        if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-      });
-  return result;
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+            tmp = item.split("=");
+            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
 }
+webcamButton.click();
 
-callInput.value = findGetParameter("answer")
-answerButton.click()
+async function answer_call() {
+    await new Promise((r) => setTimeout(r, 2000));
+    callInput.value = findGetParameter("answer");
+    answerButton.click();
+}
+answer_call();
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
+  plugins: [ mkcert() ],
+  port: 3001,
   server: { https: true },
-  plugins: [ mkcert() ]
 })
